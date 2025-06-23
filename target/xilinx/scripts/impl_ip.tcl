eb7557e2cb1f77de151a744be4e18229dffb7451
m-d-upm <milos.dordevic@upm.es>
2025-06-23 16:11:06 +0200

    modified clock config parameters

--- target/xilinx/scripts/impl_ip.tcl
+++ target/xilinx/scripts/impl_ip.tcl
@@ -25,29 +25,30 @@ switch $proj {
                     CONFIG.CLKOUT2_USED {true} \
                     CONFIG.CLKOUT3_USED {true} \
                     CONFIG.CLKOUT4_USED {true} \
-                    CONFIG.CLK_OUT1_PORT {clk_50} \
-                    CONFIG.CLK_OUT2_PORT {clk_48} \
-                    CONFIG.CLK_OUT3_PORT {clk_20} \
-                    CONFIG.CLK_OUT4_PORT {clk_10} \
-                    CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {50.000} \
-                    CONFIG.CLKOUT2_REQUESTED_OUT_FREQ {48.000} \
-                    CONFIG.CLKOUT3_REQUESTED_OUT_FREQ {20.000} \
-                    CONFIG.CLKOUT4_REQUESTED_OUT_FREQ {10.000} \
+                    CONFIG.CLK_OUT1_PORT {clk_200} \
+                    CONFIG.CLK_OUT2_PORT {clk_125} \
+                    CONFIG.CLK_OUT3_PORT {clk_125_90} \
+                    CONFIG.CLK_OUT4_PORT {clk_50} \
+                    CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {200.000} \
+                    CONFIG.CLKOUT2_REQUESTED_OUT_FREQ {125.000} \
+                    CONFIG.CLKOUT3_REQUESTED_OUT_FREQ {125.000} \
+                    CONFIG.CLKOUT4_REQUESTED_OUT_FREQ {50.000} \
+                    CONFIG.CLKOUT3_REQUESTED_PHASE {90.000} \
                     CONFIG.CLKIN1_JITTER_PS {50.0} \
-                    CONFIG.MMCM_CLKFBOUT_MULT_F {6.000} \
+                    CONFIG.MMCM_CLKFBOUT_MULT_F {5.000} \
                     CONFIG.MMCM_CLKIN1_PERIOD {5.000} \
-                    CONFIG.MMCM_CLKOUT1_DIVIDE {24} \
-                    CONFIG.MMCM_CLKOUT2_DIVIDE {25} \
-                    CONFIG.MMCM_CLKOUT3_DIVIDE {60} \
-                    CONFIG.MMCM_CLKOUT4_DIVIDE {120} \
+                    CONFIG.MMCM_CLKOUT1_DIVIDE {5} \
+                    CONFIG.MMCM_CLKOUT2_DIVIDE {8} \
+                    CONFIG.MMCM_CLKOUT3_DIVIDE {8} \
+                    CONFIG.MMCM_CLKOUT4_DIVIDE {20} \
                     CONFIG.NUM_OUT_CLKS {4} \
-                    CONFIG.CLKOUT1_JITTER {112.316} \
+                    CONFIG.CLKOUT1_JITTER {98.146} \
                     CONFIG.CLKOUT1_PHASE_ERROR {89.971} \
-                    CONFIG.CLKOUT2_JITTER {129.198} \
+                    CONFIG.CLKOUT2_JITTER {107.523} \
                     CONFIG.CLKOUT2_PHASE_ERROR {89.971} \
-                    CONFIG.CLKOUT3_JITTER {155.330} \
+                    CONFIG.CLKOUT3_JITTER {107.523} \
                     CONFIG.CLKOUT3_PHASE_ERROR {89.971} \
-                    CONFIG.CLKOUT4_JITTER {178.053} \
+                    CONFIG.CLKOUT4_JITTER {129.198} \
                     CONFIG.CLKOUT4_PHASE_ERROR {89.971} \
                     ] [get_ips $proj]
             }
